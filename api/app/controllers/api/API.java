40ad09f8f97a03f683972a040f7d619b210ec3fa
fivesmallq <fivesmallq@gmail.com>
2016-11-23 19:34:07 +0800

    update 使用play module模式开发

---
+++ api/app/controllers/api/API.java
@@ -0,0 +1,9 @@
+package controllers.api;
+
+import play.mvc.Controller;
+
+public class API extends Controller{
+      public static void index(){
+                renderText("hello, api module!");
+                    }
+}
