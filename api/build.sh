ed1a7244665211c07c30fcee171c47293ca5ff39
fivesmallq <fivesmallq@gmail.com>
2016-11-30 16:39:28 +0800

    update: add play version require

--- api/build.sh
+++ api/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MODULE="api"
 VERSION=`grep self conf/dependencies.yml | sed "s/.*$MODULE //"`
-TARGET=/var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
+TARGET=/Users/fivesmallq/nginx/html/play-$MODULE/$MODULE-$VERSION.zip
 
 rm -fr dist
 play dependencies --sync || exit $?
@@ -11,5 +11,5 @@ if [ -e $TARGET ]; then
     echo "Not publishing, $MODULE-$VERSION already exists"
 else
 	echo "copy to $TARGET"
-    #cp dist/*.zip $TARGET
+    cp dist/*.zip $TARGET
 fi
