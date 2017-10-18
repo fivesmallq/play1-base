ba031f7ca7ca372c95ed9701f54aee1dd347fc0d
fivesmallq <fivesmallq@gmail.com>
2017-10-18 21:53:11 +0800

    fix: only gzip text,json,xml format

--- api/app/controllers/api/interceptor/Gzip.java
+++ api/app/controllers/api/interceptor/Gzip.java
@@ -1,5 +1,7 @@
 package controllers.api.interceptor;
 
+import com.google.common.base.Splitter;
+import com.google.common.net.MediaType;
 import org.apache.commons.lang.StringUtils;
 import play.Play;
 import play.mvc.Controller;
@@ -9,12 +11,14 @@ import utils.Compression;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * response gzip压缩.
  */
 public class Gzip extends Controller {
     private static boolean gzipDisabled = true;
+    private static String contentTypes = Play.configuration.getProperty("gzip.contentTypes", "text,json,xml");
 
 
     static {
@@ -27,23 +31,29 @@ public class Gzip extends Controller {
     public static void compress() throws IOException {
         if (gzipDisabled && response != null && isGzipSupported(request)) {
             String contentType = response.contentType;
-            if (StringUtils.isNotEmpty(contentType) && contentType.contains("image")) {
-                return;
+            if (StringUtils.isNotEmpty(contentType)) {
+                // only gzip text,json,xml format
+                String subtype = MediaType.parse(contentType).subtype();
+                List<String> gzipContypes = Splitter.on(",").omitEmptyStrings().trimResults().splitToList(contentTypes);
+                if (!gzipContypes.contains(subtype)) {
+                    return;
+                }
+                String content = response.out.toString();
+                if ("".equals(content)) {
+                    return; // fix stange chars on suspended requests
+                }
+                final ByteArrayOutputStream gzip = Compression.getGzipStream(content);
+                response.setHeader("Content-Encoding", "gzip");
+                response.setHeader("Content-Length", gzip.size() + "");
+                response.out = gzip;
             }
-            String content = response.out.toString();
-            if ("".equals(content)) {
-                return; // fix stange chars on suspended requests
-            }
-            final ByteArrayOutputStream gzip = Compression.getGzipStream(content);
-            response.setHeader("Content-Encoding", "gzip");
-            response.setHeader("Content-Length", gzip.size() + "");
-            response.out = gzip;
         }
     }
 
     /**
      * @return Whether the request browser supports GZIP encoding
      */
+
     public static boolean isGzipSupported(final Http.Request request) {
         // key must be lower-case.
         final Http.Header encodingHeader = request.headers.get("accept-encoding");
